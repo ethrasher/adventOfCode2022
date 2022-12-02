752511a02a197ccab3166001d613716814c07adc
liz14zyt <liz14zyt@gmail.com>
2022-12-01 22:43:02 -0500

    finished and cleaned up day 1

---
+++ 01/puzzle.py
@@ -0,0 +1,25 @@
+#213159
+def readFile():
+  file1 = open('puzzleInput.txt', 'r')
+  lines = file1.readlines()
+  return list(map(formatLine, lines))
+
+def formatLine(line):
+  return line.strip() if line == "\n" else int(line.strip())
+
+def getElvesSnackTotals(snacksFromFile):
+  elvesSnackTotal = [0]
+  for snackCal in snacksFromFile:
+    if snackCal == '':
+    	elvesSnackTotal.append(0)
+    else:
+    	elvesSnackTotal[-1] += snackCal
+  return sorted(elvesSnackTotal)
+
+snacksFromFile = readFile()
+elvesSnackTotals = sorted(getElvesSnackTotals(snacksFromFile))
+# Part 1 Answer
+print("Part A: most snacks", elvesSnackTotals[-1])
+
+# Part 2 Answer
+print("Part B: 3 most snacks", sum(elvesSnackTotals[len(elvesSnackTotals)-3:]))
